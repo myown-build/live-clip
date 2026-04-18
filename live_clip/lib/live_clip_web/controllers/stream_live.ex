83b695d21f1a5c94bef83431ea07a36db222a1e5
Chris <chris@chriskski.com>
2026-04-18 11:50:19 -0600

    Remove comments

--- live_clip/lib/live_clip_web/controllers/stream_live.ex
+++ live_clip/lib/live_clip_web/controllers/stream_live.ex
@@ -48,18 +48,10 @@ defmodule LiveClipWeb.StreamLive do
     dbg(socket.assigns[:live_action])
 
     socket = assign(socket, 
-      stream_key: nil,
       client: nil
     )
-
-    # socket = load_app(socket, ref)
-
     if connected?(socket) do
       # subscribe to pubsub events from oz backend.
-
-      # socket = assign(socket, search_form: to_form(%{"query" => ""}))
-
-      # Phoenix.PubSub.subscribe(LiveClip.PubSub, "rtmp:new")
 
       {:ok, socket}
     else
@@ -67,27 +59,7 @@ defmodule LiveClipWeb.StreamLive do
     end
   end
 
-  # def handle_info({:loader, params}, socket) do
-  #   case params do
-  #     %{path: path, events: events} ->
-  #       socket = assign(socket, events: [path | socket.assigns.events])
-
-  #       {:noreply, socket}
-
-  #     _ ->
-  #       {:noreply, socket}
-  #   end
-  # end
-
   @impl true
-  # def handle_info(%{stream_key: stream_key} = event, socket) do
-  #   # handle_webrtc_msg(msg, state)
-
-  #   # dbg(event)
-
-  #   {:noreply, socket}
-  # end
-
   def handle_info(msg, socket) do
     dbg(msg)
     {:noreply, socket}
@@ -105,26 +77,6 @@ defmodule LiveClipWeb.StreamLive do
     # socket = assign(socket, search_form: to_form(%{"query" => query}))
     {:noreply, socket}
   end
-
-  # def handle_event("video:create", params, %{assigns: %{watcher_id: id}} = socket) when id !== nil do
-  #   # %{fishmeat_ref: ref} = socket.private
-  #   dbg(params)
-
-  #   case Yama.Cache.get({:watcher, id}) do
-  #     nil ->
-  #       # dbg("bad id #{id}")
-  #       {:noreply, socket}
-
-  #     true ->
-  #       dbg(socket.assigns.search_form[:query].value)
-  #       # value = socket.assigns.search_form[:query].value
-  #       # Logger.debug("Creating video #{inspect(id)}")
-  #       # Yama.Streamer.create_video(%{id: id, range: {1, 2}})
-
-  #       {:noreply, socket}
-  #   end
-  # end  
-
 
   def handle_event(event, params, socket) do
     dbg([event, params])
